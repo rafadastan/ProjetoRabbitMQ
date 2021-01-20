9e68eb415b5b1cf392f0f6bbdbe12317e1c15e8e
Raphael Augusto P <rafadastan02@live.com>
2021-01-19 22:48:31 -0300

    criando projeto rabbitmq

---
+++ ProjetoRabbitMQ/Controllers/MessagesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjetoRabbitMQ.Models;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoRabbitMQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ConnectionFactory _factory;
+        private const string QUEUE_NAME = "messages";
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+
+        [HttpPost]
+        public IActionResult PostMessage([FromBody] MessageInputModel message)
+        {
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: QUEUE_NAME,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var stringfiedMessage = JsonConvert.SerializeObject(message);
+                    var bytesMessage = Encoding.UTF8.GetBytes(stringfiedMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: bytesMessage);
+                }
+            }
+
+            return Accepted();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# PROJETO RABBITMQ
+
+Desenvolvido por Raphael Augusto Pereira de Assis
+
+#Tecnologias
+
+Microsoft.AspNetCore.Authentication.OpenIdConnect Version="3.1.11"
+Newtonsoft.Json Version="12.0.3"
+RabbitMQ.Client" Version="6.2.1"
+Swashbuckle.AspNetCore" Version="5.6.3" 
+
+
